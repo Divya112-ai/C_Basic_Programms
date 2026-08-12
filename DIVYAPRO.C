3dc7c11d02ed365d3eb19510e7653ccdc106172c
Divya112-ai <divyaprajapati1126@gmail.com>
2026-08-12 23:35:09 +0530

    Add files via upload

---
+++ DIVYAPRO.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{float a,b;
+clrscr();
+printf("the value of a =");
+scanf("%f",&a);
+b= sqrt(a);
+printf("the value of b=%f",b);
+if (b==int)
+{
+printf("a is a perfect no.");
+}
+else
+{
+printf("a is not a perfect no.");
+}
+getch();
+}
