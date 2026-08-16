e4e851cfbf7d327155e43eda26a174eb9ce40343
Divya112-ai <divyaprajapati1126@gmail.com>
2026-08-16 22:16:29 +0530

    Add files via upload

---
+++ PRO192.C
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char s1[80],s2[20];
+int len1=0,len2=0,i,j,found=0;
+clrscr();
+printf("Enter a string:>>");
+gets(s1);
+while(s1[len1]!='\0')len1++;
+printf("\nEnter the word to find:>>");
+gets(s2);
+while(s2[len2]!='\0')len2++;
+for(i=0;i<len1;i++)
+{
+	found=1;
+	for(j=0;j<len2;j++)
+	{
+		if(s1[i+j]!=s2[j])
+		{
+			found=0;
+			break;
+		}
+	}
+	if(found==1)
+	{
+		printf("\n\n\t String found");
+		break;
+	}
+}
+if(found==0)
+{
+	printf("\n\n\t String not found");
+}
+getch();
+}
